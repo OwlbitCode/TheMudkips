168bc4896339a7e103a9988f5192e01493afc43c
hermes123456 <lawscamille@gmail.com>
2019-11-02 22:19:47 -0700

    Please read below
    All my stuff got deleted by mistake, I have to redo my coaches story so I'll push that up later. Prettification will come later too since I lost all my work :(. Note: this only works once the "Stadium Capacity" field is changed to "INT" and once the commas are removed.

--- TheMudkips/nbaarenas.cpp
+++ TheMudkips/nbaarenas.cpp
@@ -6,9 +6,124 @@ nbaArenas::nbaArenas(QWidget *parent) :
     ui(new Ui::nbaArenas)
 {
     ui->setupUi(this);
+
+    //POPULATE THE item name combo box
+    QSqlQuery * qry1 = new QSqlQuery(myDB);
+    QSqlQueryModel * combo = new QSqlQueryModel();
+    qry1->prepare("SELECT `Arena Name` "
+                  "FROM 'Team Info' "
+                  "ORDER BY `Arena Name` ASC ");
+    qry1->exec();
+
+    combo->setQuery(*qry1);
+
+    ui->arenaCB->setModel(combo);
+
+    ui->arenaCB->setStyleSheet("QComboBox { background-color: LightBlue; }");
+
+    on_arenaButton_clicked(); //default view
+
+    totalCapacity(); //grand total initialized
+
+
 }
 
 nbaArenas::~nbaArenas()
 {
     delete ui;
 }
+
+void nbaArenas::on_arenaButton_clicked()
+{
+       myDB = QSqlDatabase::database();
+
+       QSqlQueryModel *model = new QSqlQueryModel;
+
+       model->setQuery("SELECT `Arena Name`, `Team Name`,  `Stadium Capacity` "
+                       "FROM 'Team Info' "
+                       "ORDER BY `Arena Name` ASC ");
+
+       if(model->lastError().isValid())
+           qDebug() << model->lastError();
+
+       ui->arenaTV->setModel(model);
+
+            // Set Table Column Width
+       ui->arenaTV->setColumnWidth(0,150);
+       ui->arenaTV->setColumnWidth(1,150);
+       ui->arenaTV->setColumnWidth(2,150);
+
+
+       // Set Table Column Header Text
+       model->setHeaderData(0, Qt::Horizontal, QObject::tr("Arena Name"));
+       model->setHeaderData(1, Qt::Horizontal, QObject::tr("Team Name"));
+       model->setHeaderData(2, Qt::Horizontal, QObject::tr("Seating Capacity"));
+
+}
+
+void nbaArenas::on_stadiumCapacityButton_clicked()
+{
+    myDB = QSqlDatabase::database();
+
+    QSqlQueryModel *model = new QSqlQueryModel;
+
+    model->setQuery("SELECT `Stadium Capacity`, `Arena Name`,`Team Name` "
+                    "FROM 'Team Info' "
+                    "ORDER BY `Stadium Capacity` ASC ");
+
+    if(model->lastError().isValid())
+        qDebug() << model->lastError();
+
+    ui->arenaTV->setModel(model);
+
+         // Set Table Column Width
+    ui->arenaTV->setColumnWidth(0,150);
+    ui->arenaTV->setColumnWidth(1,150);
+    ui->arenaTV->setColumnWidth(2,150);
+
+
+    // Set Table Column Header Text
+    model->setHeaderData(0, Qt::Horizontal, QObject::tr("Seating Capacity"));
+    model->setHeaderData(1, Qt::Horizontal, QObject::tr("Arena Name"));
+    model->setHeaderData(2, Qt::Horizontal, QObject::tr("Team Name"));
+}
+
+void nbaArenas::totalCapacity()
+{
+    total = 0;
+    QSqlQuery qry;
+    qry.prepare("SELECT * "
+                "FROM 'Team Info' "
+                "ORDER BY `Stadium Capacity` ASC ");
+    qry.exec();
+
+    while(qry.next())
+    {
+        total = total+ qry.value(5).toInt();
+        qDebug() << "total" << total;
+    }
+
+    ui->totalLabel1->setNum(total);
+}
+
+void nbaArenas::on_arenaCB_currentIndexChanged()
+{
+
+    //Update Table View based selected arena.
+    QSqlQuery * qry1 = new QSqlQuery(myDB);
+
+    QSqlQueryModel * newModel = new QSqlQueryModel();
+
+    QString name = ui->arenaCB->currentText();
+
+    qry1->prepare("SELECT `Arena Name`, `Team Name`, `Stadium Capacity` "
+                  "FROM 'Team Info' "
+                  "WHERE `Arena Name` = '"+name+"' "
+                  "ORDER BY `Arena Name` ASC ");
+    qry1->exec();
+
+    newModel->setQuery(*qry1);
+
+    ui->arenaTV->setModel(newModel);
+
+  }
--- TheMudkips/nbaarenas.h
+++ TheMudkips/nbaarenas.h
@@ -2,6 +2,12 @@
 #define NBAARENAS_H
 
 #include <QWidget>
+#include <QSqlDatabase>
+#include <QSqlQuery>
+#include <QSqlQueryModel>
+#include <QSqlError>
+#include <QDebug>
+
 
 namespace Ui {
 class nbaArenas;
@@ -15,8 +21,19 @@ public:
     explicit nbaArenas(QWidget *parent = nullptr);
     ~nbaArenas();
 
+private slots:
+    void on_arenaButton_clicked();
+
+    void on_stadiumCapacityButton_clicked();
+
+    void totalCapacity();
+
+    void on_arenaCB_currentIndexChanged();
+
 private:
     Ui::nbaArenas *ui;
+    QSqlDatabase myDB;
+    int total;
 };
 
 #endif // NBAARENAS_H
