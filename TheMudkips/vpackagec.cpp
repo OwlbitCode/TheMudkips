44222cb05247962aa5ab93fe84c562fbc701f8d6
OwlBitCode <owlbitcode@gmail.com>
2019-11-20 21:01:13 -0800

    Updating packagec

--- TheMudkips/vpackagec.cpp
+++ TheMudkips/vpackagec.cpp
@@ -1,6 +1,17 @@
 #include "vpackagec.h"
 #include "ui_vpackagec.h"
 
+/****************************************************************************
+ * CONSTRUCTOR
+ * --------------------------------------------------------------------------
+ * Displays default view for window.
+ * --------------------------------------------------------------------------
+ * PRE-CONDITIONS
+ *      Database class created to connect to database.
+ *
+ * POST-CONDITIONS
+ *      ==> Displays default view.
+ ***************************************************************************/
 vPackageC::vPackageC(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::vPackageC)
@@ -10,24 +21,48 @@ vPackageC::vPackageC(QWidget *parent) :
     // Get database connection
     myDB = QSqlDatabase::database();
 
+    // Display default combobox view which also updates the left list view
     defaultComboBoxView();
 
+    // Set right side to disabled
     ui->sortedGroup->setEnabled(false);
 }
 
+/****************************************************************************
+ * DESTRUCTOR
+ * --------------------------------------------------------------------------
+ * DELETES ui
+ * --------------------------------------------------------------------------
+ * PRE-CONDITIONS
+ *      ui exists
+ *
+ * POST-CONDITIONS
+ *      ==> Deletes ui
+ ***************************************************************************/
 vPackageC::~vPackageC()
 {
     delete ui;
 }
 
+/****************************************************************************
+ * defaultListView
+ * --------------------------------------------------------------------------
+ * Loads all teams from database query into ListWidget, except the startTeam.
+ * --------------------------------------------------------------------------
+ * PRE-CONDITIONS
+ *      database is open
+ *
+ * POST-CONDITIONS
+ *      ==> Displays teams into List Widget (teamList) on left side.
+ ***************************************************************************/
 void vPackageC::defaultListView()
 {
     // Create new database query
     QSqlQuery * qry = new QSqlQuery(myDB);
 
-    qDebug() << startTeam;
+//    qDebug() << startTeam;
 
-    // Set up the query to create ordered list of destination teams
+    // Set up the query to create ordered list of teams, except startTeam
     qry->prepare("SELECT DISTINCT `Beg Team` "
                  "FROM Distances "
                  "WHERE `Beg Team` != '"+startTeam+"'"
@@ -49,6 +84,14 @@ void vPackageC::defaultListView()
     }
 }
 
+/****************************************************************************
+ * defaultComboBoxView
+ * --------------------------------------------------------------------------
+ * Query of all teams is displayed in the combo box.
+ * --------------------------------------------------------------------------
+ * POST-CONDITIONS
+ *      ==> Displays combobox with all team names.
+ ***************************************************************************/
 void vPackageC::defaultComboBoxView()
 {
     QSqlQuery * qry1 = new QSqlQuery(myDB);
@@ -64,11 +107,20 @@ void vPackageC::defaultComboBoxView()
     teamName = ui->startTeam->currentText();
 }
 
+/****************************************************************************
+ * sortTeamList
+ * --------------------------------------------------------------------------
+ * Uses dijkstra's algorithm to create a dynamic array for team order
+ * and distance traveled using customList plus startTeam.
+ * --------------------------------------------------------------------------
+ * POST-CONDITIONS
+ *      ==> Creates sorted dynamic arrays for teams and destinations.
+ ***************************************************************************/
 // Display dynamic sorted array that will be passed in list
 void vPackageC::sortTeamList()
 {
-    qDebug() << "size: " << teamNum;
-    QString temp; // used as temporary storage while swapping
+//    qDebug() << "size: " << teamNum;
+//    QString temp; // used as temporary storage while swapping
     sortedTeams = new QString[teamNum]; // new object for dynamic
                                         // team array
     sortedDistance = new int[teamNum];  // new object for dynamic
@@ -78,20 +130,40 @@ void vPackageC::sortTeamList()
      * PROCESS: Copy contents of customList (fan's selected teams)
      *          into dynamic array.
      ***********************************************************************/
+    // Put startTeam at index 0
     sortedTeams[0] = startTeam;
     sortedDistance[0] = 0;
 
+    // Copy teams from customList to dynamic array (sortedTeams)
     for(int i = 1; i < teamNum; i++)
     {
+        // customList needs i-1 because it starts at 0, but the iteration
+        // starts at 1 because [0] is where startTeam was placed.
         sortedTeams[i] = customList.at(i-1)->text();
     }
 }
 
+/****************************************************************************
+ * on_cancelButton_clicked
+ * --------------------------------------------------------------------------
+ * Closes this window.
+ * --------------------------------------------------------------------------
+ * POST-CONDITIONS
+ *      ==> Closes this window.
+ ***************************************************************************/
 void vPackageC::on_cancelButton_clicked()
 {
     this->close();
 }
 
+/****************************************************************************
+ * on_startButton_clicked
+ * --------------------------------------------------------------------------
+ * Closes this window and opens the simulation window.
+ * --------------------------------------------------------------------------
+ * POST-CONDITIONS
+ *      ==> Closes this window and opens simulation window.
+ ***************************************************************************/
 void vPackageC::on_startButton_clicked()
 {
     simulationWindow = new vSimulation;
@@ -100,21 +172,40 @@ void vPackageC::on_startButton_clicked()
     this->close();
 }
 
+/****************************************************************************
+ * on_startTeam_currentIndexChanged
+ * --------------------------------------------------------------------------
+ * Updates startTeam to the selected item in the combobox and update
+ * teamList with all team names, excluding the startTeam.
+ * --------------------------------------------------------------------------
+ * POST-CONDITIONS
+ *      ==> Updates teamList view.
+ ***************************************************************************/
 void vPackageC::on_startTeam_currentIndexChanged(const QString &arg1)
 {
     startTeam = arg1;
-    qDebug() << startTeam;
+//    qDebug() << startTeam;
     ui->teamList->clear();
     defaultListView();
 }
 
+/****************************************************************************
+ * on_confirmButton_clicked
+ * --------------------------------------------------------------------------
+ * Stores selected teams into an array list.  Get teamNum and set
+ * startTeam.  Use sorting algorithm in sortTeamList to get dynamic
+ * arrays.  And display the results in the dynamic arrays.
+ * --------------------------------------------------------------------------
+ * POST-CONDITIONS
+ *      ==> Displays resulting dynamic arrays on right side of window.
+ ***************************************************************************/
 void vPackageC::on_confirmButton_clicked()
 {
     // Stores selected items from teamList into customList
     ui->sortedTeamList->clear();
     customList = ui->teamList->selectedItems();
     teamNum = customList.count();
-    teamNum++;
+    teamNum++; // Need to add 1 to count the startTeam
     startTeam = ui->startTeam->currentText();
 
     sortTeamList();
--- TheMudkips/vpackagec.h
+++ TheMudkips/vpackagec.h
@@ -20,32 +20,48 @@ class vPackageC : public QWidget
 
 public:
     explicit vPackageC(QWidget *parent = nullptr);
+    //!< Constructor
+
     ~vPackageC();
+    //!< Destructor
+
     void defaultListView();
+    //!< Sets default view for list on left side listing teams from db
+
     void defaultComboBoxView();
+    //!< Sets default combobox view using values from the database.
+
     void sortTeamList();
+    //!< Creates the sorted dynamic team and destination arrays
 
 private slots:
     void on_cancelButton_clicked();
+    //!< Closes window and returns fan to package window
 
     void on_startButton_clicked();
+    //!< Opens vacation simulation window and closes this window
 
     void on_startTeam_currentIndexChanged(const QString &arg1);
+    //!< Updates list view and sets starting team
 
     void on_confirmButton_clicked();
+    //!< Creates list, calls sortTeamList(), and displays arrays in list
 
 private:
-    Ui::vPackageC *ui;
-    QSqlDatabase myDB;
+    Ui::vPackageC *ui;      /*!< class object */
+    QSqlDatabase myDB;      /*!< database connection */
     vSimulation *simulationWindow;
-
+                            /*!< class object for simulation window */
     QList<QListWidgetItem *> customList;
+                            /*!< list of teams selected from list */
     QString* sortedTeams = nullptr;
+                            /*!< list of sorted teams */
     int* sortedDistance = nullptr;
-    int teamNum;            // number of teams selected
-    QString teamName;       // team name
-    QString startTeam;      // starting team name
-    QString closestTeam;    // team closest to starting team
+                            /*!< list of sorted destinations */
+    int teamNum;            /*!< number of teams selected */
+    QString teamName;       /*!< team name */
+    QString startTeam;      /*!< starting team name */
+    QString closestTeam;    /*!< team closest to starting team */
 };
 
 #endif // VPACKAGEC_H
