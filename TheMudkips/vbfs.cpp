2a42ee2bf2b464f41b3390b982f4a013b7fab333
OwlBitCode <owlbitcode@gmail.com>
2019-11-21 08:58:24 -0800

    updating bfs page
    also adding to images

--- TheMudkips/vbfs.cpp
+++ TheMudkips/vbfs.cpp
@@ -6,9 +6,221 @@ vBFS::vBFS(QWidget *parent) :
     ui(new Ui::vBFS)
 {
     ui->setupUi(this);
+
+    // get number of distinct team names in db
+    getNumTeams();
+
+    // create dynamic master list for team names
+    teamList = new std::string [numTeams];
+
+    // populate team master list from db
+    getTeamList();
+
+    // get root team index
+    rootIndex = 0;
+    bool isFound = false;
+    while(!isFound)
+    {
+        if(teamList[rootIndex] == "Los Angeles Lakers")
+            isFound = true;
+        else
+            rootIndex++;
+    }
+//    qDebug() << rootIndex;
+
+    // create dynamic adjacency matrix (pointers array)
+    adj = new float* [numTeams];
+
+    // create columns for adjacency matrix
+    for(int i = 0; i < numTeams; i++)
+    {
+        adj[i] = new float[numTeams];
+        // initialize each cell to -1
+        memset(adj[i], -1, numTeams*sizeof(float));
+    }
+
+    // populate adjacency matrix (add edges)
+    createMatrix();
+
+    BFS();
 }
 
 vBFS::~vBFS()
 {
+    delete [] teamList;
     delete ui;
+}
+
+// Gets number of distinct beginning team names from distance database
+void vBFS::getNumTeams()
+{
+    // Create new database query
+    QSqlQuery * qry = new QSqlQuery(myDB);
+
+    // Set up the query to create ordered list of teams, except startTeam
+    qry->prepare("SELECT COUNT (DISTINCT `Beg Team`) "
+                 "FROM Distances");
+
+    // Execute query if valid
+    if(qry->exec())
+    {
+        qry->next();
+        numTeams = qry->value(0).toInt();
+    }
+    else
+    {
+        qDebug() << ("bfs Error: qry failed.");
+    }
+//    qDebug() << numTeams;
+}
+
+// Create team name list, sorted in ascending
+// Used for indexing team names
+void vBFS::getTeamList()
+{
+    int index = 0;
+
+    // Create new database query
+    QSqlQuery * qry = new QSqlQuery(myDB);
+
+    // Set up the query to create ordered list of teams, except startTeam
+    qry->prepare("SELECT DISTINCT `Beg Team` "
+                 "FROM Distances "
+                 "ORDER BY `Beg Team` COLLATE NOCASE ASC ");
+
+    // Execute query if valid
+    if(qry->exec())
+    {
+        // Populating list from query
+        while(qry->next())
+        {
+            uTeam = qry->value(0).toString();
+            teamList[index] = uTeam.toStdString();
+            index++;
+        }
+    }
+    else
+    {
+        qDebug() << ("bfs getTeamList Error: qry failed.");
+    }
+}
+
+void vBFS::createMatrix()
+{
+    int sIndex = 0; // start team index value
+    int dIndex = 0; // destination team index value
+    bool isFound = false;
+
+    // Create new database query
+    QSqlQuery * qry = new QSqlQuery(myDB);
+
+    // Set up the query to create ordered list of teams, except startTeam
+    qry->prepare("SELECT * "
+                 "FROM Distances");
+
+    // Execute query if valid
+    if(qry->exec())
+    {
+        // Populating list from query
+        while(qry->next())
+        {
+            sIndex = 0; // start team index value
+            dIndex = 0; // destination team index value
+
+            // Find start team in team list for its index
+            isFound = false;
+            uTeam = qry->value(0).toString();
+            while(!isFound && sIndex < numTeams)
+            {
+                if(teamList[sIndex] == uTeam.toStdString())
+                    isFound = true;
+                else
+                    sIndex++;
+            }
+//            qDebug() << sIndex;
+
+            // Find destination team in team list for its index
+            isFound = false;
+            vTeam = qry->value(2).toString();
+            while(!isFound && dIndex < numTeams)
+            {
+                if(teamList[dIndex] == vTeam.toStdString())
+                    isFound = true;
+                else
+                    dIndex++;
+            }
+//            qDebug() << dIndex;
+
+            distance = qry->value(3).toFloat();
+//            qDebug() << distance;
+
+            // Add edge
+            adj[sIndex][dIndex] = distance;
+        }
+    }
+    else
+    {
+        qDebug() << ("vpackagec Error: qry failed.");
+    }
+}
+
+void vBFS::BFS()
+{
+    std::vector<bool> visited(numTeams,false);
+    std::vector<int> q;
+    std::vector<std::tuple<int, int, float>> temp;
+    int u, v, wt, vis;
+    QString tempStr;
+
+    q.push_back(rootIndex);
+    visited[rootIndex] = true;
+    tempStr = QString::fromStdString(teamList[rootIndex]);
+    ui->bfsList->addItem(tempStr);
+
+    while(!q.empty())
+    {
+        // get all destinations from origin
+        while(!q.empty())
+        {
+            vis = q[0];
+            q.erase(q.begin());
+            for(int i = 0; i < numTeams; i++)
+            {
+                if(adj[vis][i] >= 0 && (!visited[i]))
+                    temp.push_back(std::make_tuple(vis,i,adj[vis][i]));
+            }
+            sort(temp.begin(), temp.end(),
+                 [](std::tuple<int, int, float> const &a, std::tuple<int, int, float> &b)
+                {
+                     return (std::get<2>(a) < std::get<2>(b));
+                });
+        }
+
+        ui->bfsList->addItem("--------- Level Change ---------");
+
+        for(auto it = temp.begin(); it != temp.end(); ++it)
+        {
+            u = std::get<0>(*it);
+            v = std::get<1>(*it);
+            wt = std::get<2>(*it);
+
+//            qDebug() << u << " " << v << wt;
+
+            if(!visited[v])
+            {
+                tDistance = tDistance + wt;
+                q.push_back(v);
+                visited[v] = true;
+                tempStr = QString::fromStdString(teamList[v]);
+                ui->bfsList->addItem(tempStr);
+            }
+        }
+
+        temp.clear();
+    }
+}
+
+bool vBFS::sortedbyWeight(const std::tuple<int, int, float>& a, const std::tuple<int, int, float>& b)
+{
+    return (std::get<2>(a) < std::get<2>(b));
 }
--- TheMudkips/vbfs.h
+++ TheMudkips/vbfs.h
@@ -2,6 +2,15 @@
 #define VBFS_H
 
 #include <QWidget>
+#include <QSqlDatabase>
+#include <QSqlQuery>
+#include <QSqlQueryModel>
+#include <QSqlError>
+#include <QDebug>
+#include <string>
+#include <iostream>
+#include <vector>
+#include <algorithm>
 
 namespace Ui {
 class vBFS;
@@ -13,10 +22,40 @@ class vBFS : public QWidget
 
 public:
     explicit vBFS(QWidget *parent = nullptr);
+    //!< Constructor
+
     ~vBFS();
+    //!< Destructor
+
+    void getNumTeams();
+    //!< Gets number of teams (distinct) in the distance db
+
+    void getTeamList();
+    //!< Creates master list of distinct team names asc order
+
+    void createMatrix();
+    //!< Populates adjacency matrix with edges
+
+    void BFS();
+    //!< ...
+
+
+    bool sortedbyWeight(const std::tuple<int, int, float>& a, const std::tuple<int, int, float>& b);
 
 private:
     Ui::vBFS *ui;
+    QSqlDatabase myDB;      /*!< database connection */
+
+    QString uTeam;          /*!< origin team name            : u    */
+    QString vTeam;          /*!< destination team name       : v    */
+    float   distance;       /*!< distance between u and v    : wt   */
+    int     numTeams;       /*!< number of teams             : V    */
+    int     rootIndex;      /*!< starting team index for BFS : root */
+    int     tDistance;      // total distance
+
+    std::string *teamList;  /*!< array (master) list of team names */
+    float **adj;            /*!< adjacency matrix for weighted edges */
+
 };
 
 #endif // VBFS_H
