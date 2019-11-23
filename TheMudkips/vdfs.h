3a250c17d0cc69062213b524cf3221ddbab7440f
hermes123456 <lawscamille@gmail.com>
2019-11-23 10:25:52 -0800

    pushing up dfs

--- TheMudkips/vdfs.h
+++ TheMudkips/vdfs.h
@@ -2,6 +2,15 @@
 #define VDFS_H
 
 #include <QWidget>
+#include <QSqlDatabase>
+#include <QSqlQuery>
+#include <QSqlQueryModel>
+#include <QSqlError>
+#include <QDebug>
+#include <string>
+#include <iostream>
+#include <vector>
+#include <algorithm>
 
 namespace Ui {
 class vDFS;
@@ -14,9 +23,28 @@ class vDFS : public QWidget
 public:
     explicit vDFS(QWidget *parent = nullptr);
     ~vDFS();
+    void getNumTeams();
+    void getTeamList();
+    void createAdjList();
+    void DFS(int start);
 
 private:
     Ui::vDFS *ui;
+    QSqlDatabase myDB;      /*!< database connection */
+
+    QString uTeam;           /*!< origin team name    */
+    QString vTeam;          /*!< destination team name       : v    */
+    float   distance;       /*!< distance between u and v    : wt   */
+    int     numTeams;       /*!< number of teams             : V    */
+    int     rootIndex;      /*!< starting team index for DFS : root */
+    int     tDistance;      /*!< dfs total distance */
+
+    std::string *teamList;  /*!< array (master) list of team names */
+
+    int V;                  /*!< vertices*/
+
+    std::vector<std::pair<int,float>> * adj; //adjacency list poinnter
+
 };
 
 #endif // VDFS_H
