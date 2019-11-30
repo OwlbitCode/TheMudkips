78fe423570b214ec10ea3b5a49a536c6ea701a8d
OwlBitCode <owlbitcode@gmail.com>
2019-11-30 09:20:44 -0800

    dijstra's implementation
    still work in progress.

--- TheMudkips/vpackagec.cpp
+++ TheMudkips/vpackagec.cpp
@@ -24,6 +24,30 @@ vPackageC::vPackageC(QWidget *parent) :
     // Display default combobox view which also updates the left list view
     defaultComboBoxView();
 
+    // get number of distinct team names in db
+    getTeamTotal();
+
+    // create dynamic master list for team names
+    teamList = new std::string [teamTotal];
+
+    // populate team master list from db
+    getTeamList();
+
+    // create dynamic adjacency matrix (pointers array)
+    adj = new float* [teamTotal];
+
+    // create columns for adjacency matrix
+    for(int i = 0; i < teamTotal; i++)
+    {
+        // creates the columns in the 2d dynamic array
+        adj[i] = new float[teamTotal];
+        // initialize each cell to -1
+        memset(adj[i], -1, teamTotal*sizeof(float));
+    }
+
+    // populate adjacency matrix (add edges)
+    createMatrix();
+
     // Set right side to disabled
     ui->sortedGroup->setEnabled(false);
 }
@@ -108,6 +132,274 @@ void vPackageC::defaultComboBoxView()
 }
 
 /****************************************************************************
+ * getTeamTotal
+ * --------------------------------------------------------------------------
+ * Gets the total number of distinct beginning team names from distance db.
+ * --------------------------------------------------------------------------
+ * PRE-CONDITIONS
+ *      myDB is is open and connected
+ *
+ * POST-CONDITIONS
+ *      ==> Nothing.
+ ***************************************************************************/
+void vPackageC::getTeamTotal()
+{
+    // Create new database query
+    QSqlQuery * qry = new QSqlQuery(myDB);
+
+    // Set up the query to create ordered list of teams, except startTeam
+    qry->prepare("SELECT COUNT (DISTINCT `Beg Team`) "
+                 "FROM Distances");
+
+    // Execute query if valid
+    if(qry->exec())
+    {
+        qry->next();
+        // get total team number
+        teamTotal= qry->value(0).toInt();
+    }
+    else
+    {
+        qDebug() << ("Package C getTeamTotal Error: qry failed.");
+    }
+}
+
+/****************************************************************************
+ * getTeamList
+ * --------------------------------------------------------------------------
+ * Create team name master list, sorted in ascending order.  Their index
+ * will be used for referencing the team name associated with it.
+ * --------------------------------------------------------------------------
+ * PRE-CONDITIONS
+ *      myDB is is open and connected
+ *
+ * POST-CONDITIONS
+ *      ==> Nothing.
+ ***************************************************************************/
+void vPackageC::getTeamList()
+{
+    int index = 0;
+
+    // Create new database query
+    QSqlQuery * qry = new QSqlQuery(myDB);
+
+    // Set up the query to create ordered list of teams, except startTeam
+    qry->prepare("SELECT DISTINCT `Beg Team` "
+                 "FROM Distances "
+                 "ORDER BY `Beg Team` COLLATE NOCASE ASC ");
+
+    // Execute query if valid
+    if(qry->exec())
+    {
+        // Populating master team list from query
+        while(qry->next())
+        {
+            uTeam = qry->value(0).toString();
+            teamList[index] = uTeam.toStdString();
+            index++;
+        }
+    }
+    else
+    {
+        qDebug() << ("Package C getTeamList Error: qry failed.");
+    }
+    qDebug() << "getTeamList Done";
+}
+
+/****************************************************************************
+ * createMatrix
+ * --------------------------------------------------------------------------
+ * Creates an adjacency matrix for beginning team name as column (u),
+ * ending team name as rows (v), and the distance between them as the weight
+ * in the cell.
+ * --------------------------------------------------------------------------
+ * PRE-CONDITIONS
+ *      myDB is is open and connected
+ *
+ * POST-CONDITIONS
+ *      ==> Nothing.
+ ***************************************************************************/
+void vPackageC::createMatrix()
+{
+    int sIndex = 0; // start team index value
+    int dIndex = 0; // destination team index value
+    bool isFound = false;
+
+    // Create new database query
+    QSqlQuery * qry = new QSqlQuery(myDB);
+
+    // Set up the query to create ordered list of teams, except startTeam
+    qry->prepare("SELECT * "
+                 "FROM Distances");
+
+    // Execute query if valid
+    if(qry->exec())
+    {
+        // Populating list from query
+        while(qry->next())
+        {
+            sIndex = 0; // start team index value
+            dIndex = 0; // destination team index value
+
+            // Find start team in team list for its index
+            isFound = false;
+            uTeam = qry->value(0).toString();
+            while(!isFound && sIndex < teamTotal)
+            {
+                // check if there is a match
+                if(teamList[sIndex] == uTeam.toStdString())
+                    isFound = true;
+                else
+                    sIndex++;
+            }
+
+            // Find destination team in team list for its index
+            isFound = false;
+            vTeam = qry->value(2).toString();
+            while(!isFound && dIndex < teamTotal)
+            {
+                // check if there is a match
+                if(teamList[dIndex] == vTeam.toStdString())
+                    isFound = true;
+                else
+                    dIndex++;
+            }
+
+            distance = qry->value(3).toFloat();
+
+            // Add weighted edge into adjacency matrix
+            adj[sIndex][dIndex] = distance;
+        }
+    }
+    else
+    {
+        qDebug() << ("Package C createMatrix: qry failed.");
+    }
+
+    qDebug() << "createMatrix done";
+}
+
+/****************************************************************************
+ * doDijkstras
+ * --------------------------------------------------------------------------
+ * Creates a subgraph of shortest distance using dijkstra's algorithm.
+ * This is used to determine shortest distance algorithm for this package.
+ * --------------------------------------------------------------------------
+ * PRE-CONDITIONS
+ *      myDB is is open and connected
+ *      adj[][] : adjacency matrix was already created and has data
+ *      sTeamIndex: index of starting node.
+ *
+ * POST-CONDITIONS
+ *      ==> Nothing.
+ ***************************************************************************/
+void vPackageC::doDijkstras()
+{
+    float lowDist[teamTotal]; // The output array. dist[i] will hold the shortest
+                            // distance from src to i
+
+    bool cloudT[teamTotal]; // cloudT[i] will be true if vertex i is included in shortest
+                            // path tree or shortest distance from src to i is finalized
+
+    int parentArr[teamTotal]; // Parent array used to store the parent(index u) for child (index v)
+    parentArr[0] = 0; // set starting index to 0
+
+    // Initialize all distances as INFINITE and cloudT[] as false
+    for (int i = 0; i < teamTotal; i++)
+    {
+        lowDist[i] = 9999.9f;
+        cloudT[i] = false;
+    }
+
+    // Distance of source vertex from itself is always 0
+    lowDist[sTeamIndex] = 0;
+
+    // Find shortest path for all vertices
+    for (int count = 0; count < teamTotal - 1; count++) {
+        // Pick the minimum distance vertex from the set of vertices not
+        // yet processed. u is always equal to src in the first iteration.
+        int u = minDistance(lowDist, cloudT);
+
+        // Mark the picked vertex as processed
+        cloudT[u] = true;
+
+        // Update dist value of the adjacent vertices of the picked vertex.
+        for (int v = 0; v < teamTotal; v++)
+        {
+            // Update dist[v] only if is not in cloudT, there is an edge from
+            // u to v, and total weight of path from src to v through u is
+            // smaller than current value of dist[v]
+            if (!cloudT[v] && adj[u][v] && (lowDist[u] >= 9999.9f)
+                && (lowDist[u] + adj[u][v] < lowDist[v]))
+            {
+                lowDist[v] = lowDist[u] + adj[u][v];
+                parentArr[v] = u;//storing the parent u for child v
+            }
+        }
+    }
+
+    // print the constructed distance array
+    displaySolution(lowDist, parentArr);
+}
+
+/****************************************************************************
+ * minDistance
+ * --------------------------------------------------------------------------
+ * A utility function to find the vertex with minimum distance value, from
+ * the set of vertices not yet included in shortest path tree
+ * --------------------------------------------------------------------------
+ * PRE-CONDITIONS
+ *      NONE
+ *
+ * POST-CONDITIONS
+ *      ==> Nothing.
+ ***************************************************************************/
+int vPackageC::minDistance(float lowDist[], bool cloudT[])
+{
+    // Initialize min value
+    float min = FLT_MAX;
+    int minIndex = 0;
+
+    for (int v = 0; v < teamTotal; v++)
+        if (cloudT[v] == false && lowDist[v] <= min)
+        {
+            min = lowDist[v];
+            minIndex = v;
+        }
+
+    return minIndex;
+}
+
+void vPackageC::displaySolution(float lowDist[], int parentArr[])
+{
+    {
+        int p;
+        int c;
+
+        for (int i = 0; i < teamTotal; i++)
+        {
+            p = parentArr[i];
+            c=i;
+
+            qDebug() << QString::fromStdString(teamList[i]);
+
+            while(p!=0 && c!=0)
+            {
+                qDebug() << " <- " << QString::fromStdString(teamList[p]);
+                c = p;
+                p = parentArr[p];
+            }
+
+            if (i!=0)
+            {
+                qDebug() << " <- " << QString::fromStdString(teamList[p]);
+            }
+            qDebug() << " | " << lowDist[i] << endl;
+        }
+    }
+}
+
+/****************************************************************************
  * sortTeamNormal
  * --------------------------------------------------------------------------
  * Uses dijkstra's algorithm to create a dynamic array for team order
@@ -124,7 +416,7 @@ void vPackageC::sortTeamNormal()
 //    QString temp; // used as temporary storage while swapping
     sortedTeams = new QString[teamNum]; // new object for dynamic
                                         // team array
-    sortedDistance = new int[teamNum];  // new object for dynamic
+    sortedDistance = new float[teamNum];  // new object for dynamic
                                         // distance array
 
     /************************************************************************
@@ -161,7 +453,7 @@ void vPackageC::sortTeamOptimize()
 //    QString temp; // used as temporary storage while swapping
     sortedTeams = new QString[teamNum]; // new object for dynamic
                                         // team array
-    sortedDistance = new int[teamNum];  // new object for dynamic
+    sortedDistance = new float[teamNum];  // new object for dynamic
                                         // distance array
 
     /************************************************************************
@@ -246,6 +538,18 @@ void vPackageC::on_confirmButton_clicked()
     teamNum++; // Need to add 1 to count the startTeam
     startTeam = ui->startTeam->currentText();
 
+    // Initialize variables for finding the start team index in master list
+    bool isFound = false;
+    sTeamIndex = 0;
+    // find start team index
+    while(!isFound)
+    {
+        if(teamList[sTeamIndex] == startTeam.toStdString())
+            isFound = true;
+        else
+            sTeamIndex++;
+    }
+
     // Used to determine which algorithm to use
     // If checkbox is checked (true)
     if(ui->optimizeBox->isChecked())
@@ -259,6 +563,9 @@ void vPackageC::on_confirmButton_clicked()
         qDebug() << "Optimization: false";
         sortTeamNormal();
     }
+
+    // GENERATE DIJKSTRA'S using startTeam and adjacency matrix
+//    doDijkstras();
 
     // Load all selected, sorted teams into list view
     for(int i = 0; i < teamNum; i++)
--- TheMudkips/vpackagec.h
+++ TheMudkips/vpackagec.h
@@ -8,6 +8,7 @@
 #include <QSqlError>
 #include <QDebug>
 #include <QListWidget>
+#include <cfloat>
 #include "vsimulation.h"
 
 namespace Ui {
@@ -30,6 +31,24 @@ public:
 
     void defaultComboBoxView();
     //!< Sets default combobox view using values from the database.
+
+    void getTeamTotal();
+    //!< Gets total number of teams (distinct) in the distance db
+
+    void getTeamList();
+    //!< Creates master list of distinct team names asc order
+
+    void createMatrix();
+    //!< Populates adjacency matrix with edges
+
+    void doDijkstras();
+    //!< Does dikstra's algorithm to the adjacency matrix
+
+    int minDistance(float lowDist[], bool cloudT[]);
+    //!< Used by doDijkstras() to determine minimum distance node
+
+    void displaySolution(float lowDist[], int parentArr[]);
+    //!< Used for testing results on qDebug()
 
     void sortTeamOptimize();
     //!< Creates the sorted dynamic team and destination arrays
@@ -59,14 +78,25 @@ private:
                             /*!< class object for simulation window */
     QList<QListWidgetItem *> customList;
                             /*!< list of teams selected from list */
-    QString* sortedTeams = nullptr;
-                            /*!< list of sorted teams */
-    int* sortedDistance = nullptr;
-                            /*!< list of sorted destinations */
     int teamNum;            /*!< number of teams selected */
     QString teamName;       /*!< team name */
     QString startTeam;      /*!< starting team name */
+    int sTeamIndex;         /*!< starting team index from master list */
     QString closestTeam;    /*!< team closest to starting team */
+
+    // The two passed dynamic arrays to vSimulation
+    QString* sortedTeams = nullptr;
+                            /*!< list of sorted teams */
+    float* sortedDistance = nullptr;
+                            /*!< list of sorted destinations */
+
+    // Used for creating adjacency matrix
+    QString uTeam;          /*!< origin team name         : u */
+    QString vTeam;          /*!< destination team name    : v  */
+    float   distance;       /*!< distance between u and v : wt */
+    int teamTotal;          /*!< total number of teams    : V */
+    std::string *teamList;  /*!< array (master) list of team names */
+    float       **adj;      /*!< adjacency matrix for weighted edges */
 };
 
 #endif // VPACKAGEC_H
